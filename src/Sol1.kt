d16814fc141d753992245b81e7972cd6bb475d37
vladimirpd <vladimirpaneckin@gmail.com>
2021-10-15 15:17:44 +0700

    https://github.com/vladimirpd/solution/issues/1 #3

--- src/Sol1.kt
+++ src/Sol1.kt
@@ -3,9 +3,17 @@ import test.SolutionTest
 fun main(args: Array<String>) {
 
 
-    if (args.isNotEmpty())
-        args[0].split(" ").toTypedArray().forEach {
-            println(it)
-        }
+    sol(args[0]).forEach {
+        println(it)
+
+    }
+
+
 }
+
+val sol: (String) -> Array<String> = { args ->
+     args.split(" ").toTypedArray()
+}
+
+
 
