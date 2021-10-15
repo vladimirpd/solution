42d243289121c3bcd5aa4ca7a09b1dfd33a33e04
vladimirpd <vladimirpaneckin@gmail.com>
2021-10-15 15:20:04 +0700

    https://github.com/vladimirpd/solution/issues/1 #5

--- src/Sol3.kt
+++ src/Sol3.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     if (args.isNotEmpty())
-        args[0].split(" ").toTypedArray().sorted().toSet().forEach {
+        args[0].split(" ").sorted().distinct().forEach {
             println(it)
         }
 }
