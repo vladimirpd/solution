a5a8b7c7d85ad81b4226121b8a182e7fe66b260b
vladimirpd <vladimirpaneckin@gmail.com>
2021-10-21 14:32:30 +0700

    #6 https://github.com/vladimirpd/solution/issues/1

--- src/Sol4.kt
+++ src/Sol4.kt
@@ -1,14 +1,4 @@
 fun main(args: Array<String>) {
-    val list = args[0].split(" ").toTypedArray().sorted()
-    val map = mutableMapOf<String, Int>()
-    list.forEach {
-        if (map.containsKey(it))
-            map[it]?.plus(1)?.let { it1 -> map.replace(it, it1) }
-        else map[it] = 1
-    }
-
-    map.forEach {
-        println(it)
-    }
+    args[0].split(" ").sorted().groupingBy { it }.eachCount().map { "${it.key} = ${it.value}"}.forEach(::println)
 
 }
