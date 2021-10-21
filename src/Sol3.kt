4756f78c0929d783bcfb6a1367fa7b928b8c9f89
vladimirpd <vladimirpaneckin@gmail.com>
2021-10-21 13:35:12 +0700

    #5 https://github.com/vladimirpd/solution/issues/1

--- src/Sol3.kt
+++ src/Sol3.kt
@@ -1,6 +1,4 @@
 fun main(args: Array<String>) {
     if (args.isNotEmpty())
-        args[0].split(" ").sorted().distinct().forEach {
-            println(it)
-        }
+        args[0].split(" ").sorted().distinct().forEach(::println)
 }
