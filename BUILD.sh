3cd53b365d52d297e764476abfe2a8c3280f36a0
vladimirpd <vladimirpaneckin@gmail.com>
2021-10-27 14:04:00 +0700

    #9 https://github.com/vladimirpd/solution/issues/1

---
+++ BUILD.sh
@@ -0,0 +1,7 @@
+
+kotlinc src/Sol1.kt -include-runtime -d Sol1.jar
+kotlinc src/Sol2.kt -include-runtime -d Sol2.jar
+kotlinc src/Sol3.kt -include-runtime -d Sol3.jar
+kotlinc src/Sol4.kt -include-runtime -d Sol4.jar
+kotlinc src/Sol5.kt -include-runtime -d Sol5.jar
+kotlinc src/Sol6.kt -include-runtime -d Sol6.jar
