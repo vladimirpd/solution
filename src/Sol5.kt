dc2f39195906f4e0797bada10574e6a838f219d1
vladimirpd <vladimirpaneckin@gmail.com>
2021-10-27 13:52:33 +0700

    #7 https://github.com/vladimirpd/solution/issues/1

--- src/Sol5.kt
+++ src/Sol5.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     args[0].split(" ").sorted().groupingBy { it }.eachCount()
         .toList().sortedByDescending { (_, value) -> value }.toMap()
-        .forEach { println(it.key + " " + it.value) }
+        .forEach { println(it.key + "=" + it.value) }
+
 }
