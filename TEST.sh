068cd6e64c4aa0c6c38111a60f294be8c41face4
vladimirpd <vladimirpaneckin@gmail.com>
2021-11-03 12:19:20 +0700

    #10 https://github.com/vladimirpd/solution/issues/1

--- TEST.sh
+++ TEST.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-  params="the quick brown fox jumps over the lazy dog"
+    params="the quick brown fox jumps over the lazy dog"
     realized=0
     failed=0
-    expected[1]="the\nquick\nbrown\nfox\njumps\nover\nthe\nlazy\ndog"
-    expected[2]="brown\ndog\nfox\njumps\nlazy\nover\nquick\nthe\nthe"
-    expected[3]="brown\ndog\nfox\njumps\nlazy\nover\nquick\nthe"
-    expected[4]="the=2\nquick=1\nbrown=1\nfox=1\njumps=1\nover=1\nlazy=1\ndog=1"
-    expected[5]="the=2\nbrown=1\ndog=1\nfox=1\njumps=1\nlazy=1\nover=1\nquick=1"
-    expected[6]="the=2\nbrown=1\ndog=1\nfox=1\njumps=1\nlazy=1\nover=1\nquick=1"
+    n=$'\n'
+    expected[1]="the${n}quick${n}brown${n}fox${n}jumps${n}over${n}the${n}lazy${n}dog"
+    expected[2]="brown${n}dog${n}fox${n}jumps${n}lazy${n}over${n}quick${n}the${n}the"
+    expected[3]="brown${n}dog${n}fox${n}jumps${n}lazy${n}over${n}quick${n}the"
+    expected[4]="the=2${n}quick=1${n}brown=1${n}fox=1${n}jumps=1${n}over=1${n}lazy=1${n}dog=1"
+    expected[5]="the=2${n}brown=1${n}dog=1${n}fox=1${n}jumps=1${n}lazy=1${n}over=1${n}quick=1"
+    expected[6]="the=2${n}brown=1${n}dog=1${n}fox=1${n}jumps=1${n}lazy=1${n}over=1${n}quick=1"
 
     for i in 1 2 3 4 5 6
     do
       current=$(java -jar ./Sol$i.jar $params)
-      printf "\nSol$i is testing\nExpected: \n${expected[i]}"
-      printf "\n\nCurrent:\n"
+      printf "${n}Sol$i is testing${n}Expected: ${n}${expected[i]}"
+      printf "${n}${n}Current:${n}"
       echo "$current"
-      printf "\n\n\n"
+      printf "${n}${n}${n}"
           if [[ "${expected[${i}]}" == "$current" ]]; then
               (( realized += 1 ))
               else
